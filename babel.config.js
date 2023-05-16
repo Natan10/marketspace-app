98a1350ffa99e0cc0bf7a253fed58857f660241e
Natan moreira <natan-moreira1@hotmail.com>
2023-05-16 08:30:10 -0300

    add babel plugin resolver import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				require.resolve('babel-plugin-module-resolver'),
+				{
+					root: ["./src/"],
+					alias: {
+						"@dtos": "./src/dtos",
+						"@components": "./src/components",
+						"@routes": "./src/routes",
+						"@screens": "./src/screens",
+						"@utils": "./src/utils",
+						"@assets": "./src/assets",
+						"@contexts": "./src/contexts",
+						"@hooks": "./src/hooks",
+					}
+				}
+			]
+		]
   };
 };
