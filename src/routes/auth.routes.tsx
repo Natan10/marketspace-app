c5f4b506e3cac399aa3f86e2c39466b1291cc8eb
Natan moreira <natan-moreira1@hotmail.com>
2023-05-21 22:23:35 -0300

    config tab bottom

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,20 +1,61 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'native-base';
+import { House, SignOut, Tag } from 'phosphor-react-native';
+
 import { Home } from '@screens/Home';
+import { MyAnnoucements } from '@screens/MyAnnoucements';
 
 type AuthRoutesParams = {
 	home: undefined;
 	myAnnouncement: undefined;
+	exit: undefined;
 }
 
 
 const { Navigator, Screen } = createBottomTabNavigator<AuthRoutesParams>();
 
 export function AuthRoutes(){
+	const theme = useTheme();
+
 	return(
-		<Navigator>
+		<Navigator
+			screenOptions={{
+				headerShown: false,
+				tabBarShowLabel: false,
+				tabBarActiveTintColor: theme.colors.gray[200],
+				tabBarInactiveTintColor: theme.colors.gray[400],
+				tabBarStyle: {
+					backgroundColor: theme.colors.gray[700],
+					height: 80,
+					paddingTop: 20,
+					paddingBottom: 28,
+				}
+			}}
+		>
 			<Screen 
 				name='home'
 				component={Home}
+				options={{
+					tabBarIcon: ({color}) => {
+						return <House weight="bold" color={color} size={24} />
+					} 
+				}}
+			/>
+			<Screen 
+				name='myAnnouncement'
+				component={MyAnnoucements}
+				options={{
+					tabBarIcon: ({color}) => {
+						return <Tag weight="bold" color={color} size={24} />
+					}
+				}}
+			/>
+			<Screen 
+				name='exit'
+				component={Home}
+				options={{
+					tabBarIcon: () => <SignOut size={24} weight="bold" color={theme.colors.red[400]} />
+				}}
 			/>
 		</Navigator>
 	)
